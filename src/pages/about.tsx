e9c9523aa7eebff7d1be83be25b085520d89b887
EMTDavid <EMT_David_Han@icloud.com>
2024-11-20 19:02:40 -0600

    I can sing better

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,14 +11,14 @@ const About = () => {
         </h1>
         <p className="mt-4 text-lg text-gray-600 text-center max-w-2xl">
           Hi! My name is David Han. I'm a student majoring in Mathematics, Philosophy, and Japanese at University of Wisconsin-Madison.
-          I'm interested in <span className="text-indigo-600 font-semibold"> Metaphysics</span>—, and <span className="text-indigo-600 font-semibold"> Mathematical Logic</span>
-          - the study of 
+          I'm interested in <span className="text-indigo-600 font-semibold"> Metaphysics</span>— the study of the fundamentals of beings, effects, and causation, and <span className="text-indigo-600 font-semibold"> Mathematical Logic</span>
+          - the study of formulating all mathematical concepts from pure logic.
         </p>
         <p className="mt-4 text-lg text-gray-600 text-center max-w-2xl">
           Besides academic, I'm interested in Japanese culture, specificlly reading mangas and cooking Japanese. I also play a lot of Counter-Strike 2, currently the In-Game Leader for Badger CS2 team.
         </p>
         <p className="mt-4 text-lg text-gray-600 text-center max-w-2xl">
-          I believe in the beauty of elegant thought and the timeless wisdom that shapes our understanding of the world,
+          I believe in the beauty of elegant thought and the timeless wisdom that shapes our understanding of the world.
         </p>
       </main>
       <Footer />

3e321d5c757d31a0d683648f4c5807f51c2a3a9b
EMTDavid <EMT_David_Han@icloud.com>
2024-11-20 19:08:03 -0600

    please vercel renders this

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,9 +10,7 @@ const About = () => {
           About Me
         </h1>
         <p className="mt-4 text-lg text-gray-600 text-center max-w-2xl">
-          Hi! My name is David Han. I'm a student majoring in Mathematics, Philosophy, and Japanese at University of Wisconsin-Madison.
-          I'm interested in <span className="text-indigo-600 font-semibold"> Metaphysics</span>— the study of the fundamentals of beings, effects, and causation, and <span className="text-indigo-600 font-semibold"> Mathematical Logic</span>
-          - the study of formulating all mathematical concepts from pure logic.
+          Hi! My name is David Han. I'm a student majoring in Mathematics, Philosophy, and Japanese at University of Wisconsin-Madison. I'm interested in <span className="text-indigo-600 font-semibold"> Metaphysics</span> -- the study of the fundamentals of beings, effects, and causation, and <span className="text-indigo-600 font-semibold"> Mathematical Logic</span> -- the study of formulating all mathematical concepts from pure logic.
         </p>
         <p className="mt-4 text-lg text-gray-600 text-center max-w-2xl">
           Besides academic, I'm interested in Japanese culture, specificlly reading mangas and cooking Japanese. I also play a lot of Counter-Strike 2, currently the In-Game Leader for Badger CS2 team.
